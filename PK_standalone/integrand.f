0546920a6449c0bef7b94da5aa6f69701293df40
nirajkoirala1997 <nirajkoirala1997@gmail.com>
2024-03-15 19:11:50 +0530

    Many modifications are made in integrand with loop by sir

--- PK_standalone/integrand.f
+++ PK_standalone/integrand.f
@@ -1,10 +1,13 @@
       function flo2_PK(yy,vwgt)
       implicit double precision (a-h,o-z)
-      dimension yy(10)
+      dimension yy(10),zz(10)
       dimension f1(-6:6),f2(-6:6),xl(15)
-      dimension p1(0:3),p2(0:3),p3(0:3),p4(0:3),q(0:3),xp1(0:3),xp2(0:3)
-     .          ,p(0:3,1:4),Born(1:2)
+      dimension p1(0:3),p2(0:3),p3(0:3),p4(0:3),xp1(0:3),xp2(0:3)
+     .          ,p(0:3,1:4),Born(1:2),all_PS(0:3,1:4,0:2)
       dimension SumP(1:2),SumK(1:2)
+      dimension pa1(0:3),pa2(0:3),pa3(0:3),pa4(0:3)
+      dimension pb1(0:3),pb2(0:3),pb3(0:3),pb4(0:3)
+      dimension xinvmass(0:2)
       parameter (pi=3.14159265358979d0)
       parameter (hbarc2=389.3856741D+6)
       character*50 name
@@ -14,13 +17,20 @@
       common/leg_choice/leg
       common/usedalpha/AL,ge
       common/distribution/xq
+      external PqqP,Pqqreg,Born_uU2eE
+      external AKbarP_qq,AKbarreg_qq,AKbarD_qq
+      external AKtilP_qq,AKtilreg_qq,AKtilD_qq
+      external aKbar_gq,aKtil_gq,Pgq_reg
 
       xa     = yy(1)
       xb     = yy(2)
+      xc     = yy(c)
       xmin   = 0.0d0
-      xmax   = 1.0d0 - 1d-5
+      xmax   = 1.0d0 - 1d-3
       xjac   = (xmax-xmin)
       x      = xmin+ xjac*yy(4)
+      yy(4)  = x
+
       sp     = xa*xb*s
       rsp    = dsqrt(sp) 
       xjac = xjac*2.0d0
@@ -30,130 +40,120 @@
       xlow = xq - eps
       xhigh = xq + eps
 
-        call kinvar2(yy,xinvmass,p1,p2,p3,p4)
 
-            do i=0,3
-             xp1(i) = x*p1(i)
-             xp2(i) = x*p2(i)
-            enddo
+c     all_PS -> (4momenta,particleID,kinematic type)
+c        call kinvar2_PK(yy,xinvmass,all_PS)
 
-        call p1d_to_p2d_4(p1,p2,p3,p4,p)
+        flo2_PK  = 0.0d0
+        PKplus_x = 0.0d0
+        PKplus_1 = 0.0d0
+        PKReg    = 0.0d0
+        PKDel    = 0.0d0
 
-        scalex2 = 2.0d0*x*dot(p1,p2)
-        scalex = dsqrt(scalex2)
+        sig1 = 0.0d0
+        sig2 = 0.0d0
+        sig3 = 0.0d0
+        sig4 = 0.0d0
+
+        do k = 1,2
+
+        do iplus = 0,1
+
+        if (iplus .eq. 1) then
+
+        if (k .eq. 1) then
+        call kinvar2_PK(x*xa,xb,xc,xxinvmass,p1,p2,p3,p4)
+        elseif (k .eq. 2) then
+        call kinvar2_PK(xa,x*xb,xc,xxinvmass,p1,p2,p3,p4)
+        endif
+
+        elseif (iplus .eq. 0) then
+        call kinvar2_PK(xa,xb,xc,xxinvmass,p1,p2,p3,p4)
+        endif
+
+        scale = xxinvmass
+
         pin  = 0.5d0*rsp
         flux_1 = 4.0d0*pin*rsp
         flux_x = 4.0d0*x*pin*rsp
+        coef = Born_uU2eE(0,p1,p2,p3,p4)
 
-        scale2 = 2.0d0*dot(p1,p2)
-        scale = dsqrt(scale2)
 
-      flo2_PK  = 0.0d0
-      PKplus_x = 0.0d0
-      PKplus_1 = 0.0d0
-      PKRegDel = 0.0d0
-      PKplus   = 0.0d0
+        if (scale .ge. xlow .and. scale .le. xhigh) then   
 
-c        goto 101
-c       Block for + distribution x = x
-        if ( (scalex .ge. xlow .and. scalex .le. xhigh) 
-c     .     .and.  (scale .ge. xlow .and. scale .le. xhigh)
-     .       )  then   
+             xmuf = scale
+             xmur = xmuf
+            xmuf2 = xmuf*xmuf 
+c               AL = alphasPDF(xmur)
+c              ALP = AL/2d0/Pi
+              s12 = 2d0*dot(p1,p2)
 
-            xmuf = scale
-            xmur = scale
-            ALP = AL/2d0/Pi
-            AL = alphasPDF(xmur)
-c            AS = 1.0d0
-c            ALP = AS*16.0d0/27.0d0
-
-            call getPK(1,x,xmuf,p,xp1,xp2,SumP,SumK)
-                
             call pdf(xa,xmuf,f1)
             call pdf(xb,xmuf,f2)
             call setlum(f1,f2,xl)
 
-c            sig1 = xl(1)* (SumP(1)+SumK(1))  !  [qq lum]
-c            sig1 = xl(1)* SumP(1)  !  [qq lum]
-            sig1 = xl(1)* SumP(1) *(-1.0d0)*dlog(xmuf2/s12/x)
+
+            call getPK(x,xmuf,p1,p2,p3,p4,SPKplus,SPKReg,SPK1,SPKDel)
+                
+c           coefPterm   = (-1.0d0)*dlog(xmuf2/s12/x)
+c           SumP(1) = coefPterm*PqqP(x) 
+
+c           SumK(1) = AKbarP_qq(x)+AKtilP_qq(x)
+
+c            sig1 = xl(1)*( SumP(1) + SumK(1) ) 
+
+            if (iplus .eq. 1) then
+            sig1 = xl(1)*(SumP + SumK) 
+            sig2 = xl(1)*Reg
+            elseif (iplus .eq. 0) then
+            sig3 = xl(1)*Plus_1
+            sig4 = xl(1)*Del
+            endif
   
-            sig = Alp*sig1
+            sig = Alp*sig1*coef
 
             azmth = 2.0d0*pi
             pf = 0.5d0*rsp
             ps2 = 1.0d0/(4.d0*pi*pi)*(pf/4.d0/rsp)*azmth
 
-            wgt_x = sig/flux_x*ps2*xjac*vwgt
-            PKplus_x = xnorm*wgt_x/vwgt/2d0/eps
 
-c         PKplus = PKplus_x - PKplus_1
-c              xnorm=hbarc2/16d0/pi/(xa*xb*s)
-c            write(*,*)'PKplus =', sig,SumP(1),PKplus_x
-         endif
+            if (iplus .eq. 1) then
+            wgt1 = sig1/flux_x*ps2*xjac*vwgt
+            wgt2 = sig2/flux_x*ps2*xjac*vwgt
+            PKplus_x(k) = xnorm*wgt1/vwgt/2d0/eps
+            PKReg(k) = xnorm*wgt2/vwgt/2d0/eps
 
-c 101    continue
+           elseif (iplus .eq.0) then
+            wgt3 = sig3/flux_x*ps2*xjac*vwgt
+            wgt4 = sig4/flux_x*ps2*xjac*vwgt
+            PKplus_1(k)= xnorm*wgt3/vwgt/2d0/eps
+            PKDel(k) = xnorm*wgt4/vwgt/2d0/eps
+            endif
+            enddo
 
-c        goto 102
+            PK(k) = PKplus_x - PKplus1 + PKReg + PKDel
+            enddo
 
-        if (scale .ge. xlow .and. scale .le. xhigh)  then   
+            flo2_PK = PK1(1) + PK(2)
 
-            xmuf = scale
-            xmur = scale
-            AL = alphasPDF(xmur)
-            AS = 1.0d0
-            ALP = AS*16.0d0/27.0d0
-
-            call getPK(1,x,xmuf,p,xp1,xp2,SumP,SumK)
-                
-            call pdf(xa,xmuf,f1)
-            call pdf(xb,xmuf,f2)
-            call setlum(f1,f2,xl)
-
-c            sig1 = xl(1)* (SumP(1)+SumK(1))  !  [qq lum]
-            sig1 = xl(1)* SumP(1)  !  [qq lum]
-
-            sig = Alp*sig1
-
-            azmth = 2.0d0*pi
-            pf = 0.5d0*rsp
-            ps2 = 1.0d0/(4.d0*pi*pi)*(pf/4.d0/rsp)*azmth
-
-            wgt = sig/flux_1*ps2*xjac*vwgt
-            PKplus_1 = xnorm*wgt/vwgt/2d0/eps
-
-c        write(*,*)'PKplus =', PKplus
-         endif
-
-c  102     continue
-
-         PKplus = PKplus_x - PKplus_1
-c         PKplus = PKplus_1
-
-c        if (scalex .ge. xlow .and. scalex .le. xhigh) then
-c
-c            xmuf = scalex
-c            xmur = scalex
-c            AL = alphasPDF(xmur)
-c
-c            call getPK(0,x,xmuf,p,xp1,xp2,SumP,SumK)
-c                
-c            call pdf(xa,xmuf,f1)
-c            call pdf(xb,xmuf,f2)
-c            call setlum(f1,f2,xl)
-
-c            sig1 = xl(1)* (SumP(1)+SumK(1))  !  [qq lum]
-c            sig2 = xl(8)* (SumP(2)+SumK(2))  !  [gq lum]
-c
-c            sig = sig1  !+ sig2    !  ~~~~~[ gq is turned off ]
-c            xnorm=hbarc2/16d0/pi/sp
-c            wgt = xnorm*sig*vwgt
-c            PKRegDel = wgt/vwgt/2d0/eps
-c         endif
-
-c         flo2_PK = PKplus + PKRegDel
-         flo2_PK = PKplus
       return
       end
+
+c---------------------------------------------------------------------
+      subroutine momenta_giver(kin_type,all_PS,p1,p2,p3,p4)
+      implicit double precision (a-h,o-z)
+c     all_PS(4momenta,particleID,kinematic type)
+       dimension all_PS(0:3,1:4,0:2)
+       dimension p1(0:3),p2(0:3),p3(0:3),p4(0:3)
+         do j=0,3
+           p1(j) = all_PS(j,1,kin_type) 
+           p2(j) = all_PS(j,2,kin_type) 
+           p3(j) = all_PS(j,3,kin_type) 
+           p4(j) = all_PS(j,4,kin_type) 
+         enddo
+      end
+c---------------------------------------------------------------------
+ 
 
 c---------------------------------------------------------------------
 c     [u U -> e E]  Born 
