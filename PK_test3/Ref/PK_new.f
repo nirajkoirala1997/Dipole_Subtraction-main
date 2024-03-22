9a5c1b996f14b48975a058a41fab26c5f8c9f50d
nirajkoirala1997 <nirajkoirala1997@gmail.com>
2024-03-22 12:31:07 +0530

    Started working on actual +dist, working PK_test3 created

--- PK_test3/Ref/PK_new.f
+++ PK_test3/Ref/PK_new.f
@@ -3,7 +3,7 @@
 !                                               |    process               
 ! Kb = Kbar      
 c----------------------------------------------------------- 
-      subroutine getPKPlus(x,xmuf,p,xp1,xp2,SumP)
+      subroutine getPK(iplus,x,xmuf,p,xp1,xp2,SumP,SumK)
       implicit double precision (a-h,o-z)
       parameter (pi=3.14159265358979d0)
       dimension AllP(1:4),AllK(1:4),SumP(1:2),SumK(1:2)
@@ -16,10 +16,10 @@ c      common /usedalpha/ AL,ge
       external AKtilP_qq,AKtilreg_qq,AKtilD_qq
       external aKbar_gq,aKtil_gq,Pgq_reg 
 
-       SumP(1) = 0d0
-       SumK(1) = 0d0
-       SumP(2) = 0d0
-       SumK(2) = 0d0
+c       SumP(1) = 0d0
+c       SumK(1) = 0d0
+c       SumP(2) = 0d0
+c       SumK(2) = 0d0
 
       call p2d_to_p1d_4(p,p1,p2,p3,p4)
         s12 = 2d0*dot(p1,p2)
@@ -30,126 +30,74 @@ c        Alp = Al/2.0d0/pi
         xmuf2 = xmuf*xmuf
 
       do k = 1,2
+      if (k .eq. 1) then       ! Cloice for [Leg1]       
+      Bornx = Born_uU2eE(0,xp1,p2,p3,p4)
+      elseif (k .eq. 2) then   ! Cloice for [Leg1]       
+      Bornx = Born_uU2eE(0,p1,xp2,p3,p4)
+      endif
+
+c        coefx = (-1.0d0)*dlog(xmuf2/s12/x)    !{a,ai,i} => {q,q,g}
+c        coefx = coefx*Bornx
+        coefx = Bornx
 
 c        coef1 = (-1.0d0)*dlog(xmuf2/s12)      !{a,ai,i} => {q,q,g}
-        Born = Born_uU2eE(0,p1,p2,p3,p4)
+        Born1 = Born_uU2eE(0,p1,p2,p3,p4)
 c        coef1 = coef1*Born1 
-        coef = Born 
+        coef1 = Born1 
 
-        ALLP(k) = Alp*(PqqP(x)+AKbarP_qq(x)+AKtilP_qq(x))*coef
-c        ALLP(k) = Alp*PqqP(x)*coef
+        if (iplus .eq. 1) then
+        ALLP(k) = Alp*PqqP(x)*(coefx - coef1)
 
+c        if (x .lt. 0.9d0) then
+c        write(*,*)'PqqP =', x,PqqP(x),coefx, coef1,AllP(k)
+c        endif
+
+c        Aplus= (AKbarP_qq(x)+AKtilP_qq(x))*(Bornx-Born1)
+c       Areg = 0.0d0
+c       ADel = 0.0d0
+
+c       elseif (iplus .eq. 0) then
+c       ALLP(k) = 0.0d0
+c       Aplus=  0.0d0
+c       Areg = (AKbarreg_qq(x)+AKtilreg_qq(x))*Bornx
+c       ADel = (AKbarD_qq(x)+AKtilD_qq(x))*Born1
+
+       endif
+
+c       AllK(k)= Alp*(Aplus+Areg+ADel)
+c
+c
+c       AllP(5-k) = Alp*Bornx*Pgq_reg(x)*coefx       
+c       AllK(5-k) = Alp*Bornx*( aKbar_gq(x) - (-1d0)*aKtil_gq(x) )
 
        enddo
 
        SumP(1) = AllP(1)+AllP(2)
 
-      return
-      end
-cc----------------------------------------------------------- !Misc-functions
-      subroutine getPKReg(x,xmuf,p,xp1,xp2,SumP)
-      implicit double precision (a-h,o-z)
-      parameter (pi=3.14159265358979d0)
-      dimension AllP(1:4),AllK(1:4),SumP(1:2),SumK(1:2)
-      dimension  p1(0:3),p2(0:3),p3(0:3),p4(0:3),p(0:3,1:4)
-      dimension  xp1(0:3),xp2(0:3)
-c      common /usedalpha/ AL,ge 
-      external Born_uU2eE
-      external PqqP,Pqqreg
-      external AKbarP_qq,AKbarreg_qq,AKbarD_qq
-      external AKtilP_qq,AKtilreg_qq,AKtilD_qq
-      external aKbar_gq,aKtil_gq,Pgq_reg 
-
-       SumP(1) = 0d0
-       SumK(1) = 0d0
-       SumP(2) = 0d0
-       SumK(2) = 0d0
-
-      call p2d_to_p1d_4(p,p1,p2,p3,p4)
-        s12 = 2d0*dot(p1,p2)
-        Cf = 4d0/3d0                      
-        Tr = 0.5d0
-c        Alp = Al/2.0d0/pi
-        Alp = 1.0d0
-        xmuf2 = xmuf*xmuf
-
-      do k = 1,2
-
-c        coef1 = (-1.0d0)*dlog(xmuf2/s12)      !{a,ai,i} => {q,q,g}
-        Born = Born_uU2eE(0,p1,p2,p3,p4)
-c        coef1 = coef1*Born1 
-        coef = Born 
-
-       Areg = (AKbarreg_qq(x)+AKtilreg_qq(x))*coef
-       AllP(k)= Alp*Areg
-
-       enddo
-
-       SumP(1) = AllP(1)+AllP(2)
+c      {1,2   qq channel   }   {3,4  gq channel  }
+c       do i =1,2               !~~~~~~~~[ run this loop till 4 to include gq channel also ]
+c       SumP(1) = SumP(1) + AllP(i)
+c       SumK(1) = SumK(1) + AllK(i)
+c       SumP(2) = SumP(2) + AllP(5-i)
+c       SumK(2) = SumK(2) + AllK(5-i)
+c       enddo
 
       return
       end
-cc----------------------------------------------------------- !Misc-functions
-       subroutine getPKDel(x,xmuf,p,xp1,xp2,SumP)
-      implicit double precision (a-h,o-z)
-      parameter (pi=3.14159265358979d0)
-      dimension AllP(1:4),AllK(1:4),SumP(1:2),SumK(1:2)
-      dimension  p1(0:3),p2(0:3),p3(0:3),p4(0:3),p(0:3,1:4)
-      dimension  xp1(0:3),xp2(0:3)
-c      common /usedalpha/ AL,ge 
-      external Born_uU2eE
-      external PqqP,Pqqreg
-      external AKbarP_qq,AKbarreg_qq,AKbarD_qq
-      external AKtilP_qq,AKtilreg_qq,AKtilD_qq
-      external aKbar_gq,aKtil_gq,Pgq_reg 
-
-       SumP(1) = 0d0
-       SumK(1) = 0d0
-       SumP(2) = 0d0
-       SumK(2) = 0d0
-
-      call p2d_to_p1d_4(p,p1,p2,p3,p4)
-        s12 = 2d0*dot(p1,p2)
-        Cf = 4d0/3d0                      
-        Tr = 0.5d0
-c        Alp = Al/2.0d0/pi
-        Alp = 1.0d0
-        xmuf2 = xmuf*xmuf
-
-      do k = 1,2
-
-c        coef1 = (-1.0d0)*dlog(xmuf2/s12)      !{a,ai,i} => {q,q,g}
-        Born = Born_uU2eE(0,p1,p2,p3,p4)
-c        coef1 = coef1*Born1 
-        coef = Born 
-
-       ADel = (AKbarD_qq(x)+AKtilD_qq(x))*coef
-c       ADel = AKbarD_qq(x)*coef
-c       ADel = AKtilD_qq(x)*coef
-       AllP(k)= Alp*ADel
-c       AllP(k)= Alp*coef
-c       print*,AKbarD_qq(x)+AKtilD_qq(x)
-
-       enddo
-
-       SumP(1) = AllP(1)+AllP(2)
-
-      return
-      end
-cc----------------------------------------------------------- !Misc-functions
+c----------------------------------------------------------- !Misc-functions
                                                             
       double precision function PqqP(x)
       implicit double precision (a-h,o-z)
       data PI/3.141592653589793238462643D0/
 
       CF = 4.0d0/3.0d0
-      PqqP = CF*(1.0d0+x*x)/(1.0d0-x)
+c      PqqP = CF*(1.0d0+x*x)/(1.0d0-x)
 
-c      XM=1.0D0-X
-c      DLOGXM= DLOG(XM)
-c      XD1=DLOGXM/XM
-c      CQQB1PLUS=16.0D0*XD1
-c      PqqP=CF*CQQB1PLUS
+      XM=1.0D0-X
+      LOGXM= DLOG(XM)
+      XD1=LOGXM/XM
+      CQQB1PLUS=16.0D0*XD1
+      PqqP=2.0D0*PI/3.0d0*CF*CQQB1PLUS
       return
       end
 
--- PK_test3/integrand.f
+++ PK_test3/integrand.f
@@ -118,7 +118,7 @@ c        goto 102
             ALP = AS
 
             call getPKPlus(x,xmuf,p,xp1,xp2,SumP)
-            call getPKDel(x,xmuf,p,xp1,xp2,SumK)
+c            call getPKDel(x,xmuf,p,xp1,xp2,SumK)
                 
             call pdf(xa,xmuf,f1)
             call pdf(xb,xmuf,f2)
@@ -126,30 +126,30 @@ c        goto 102
 
             sig1 = xl(1)* SumP(1)  !  [qq lum]
 c            if (x .eq. delta2)  sig2 = xl(1)* SumK(1)  !  [qq lum]
-            if (x .ge. 1d0-1d-4 ) sig2 = xl(1)* SumK(1)  
+c            if (x .ge. 1d0-1d-4 ) sig2 = xl(1)* SumK(1)  
 c            if (x .ge. 0.97 .and. x .le. 0.999) sig2 = xl(1)* SumK(1)  
 c            if (x .ge. 1d0-1d-4)  print*,"x1:",x
 c            sig2 = SumK(1)  !  [qq lum]
 
             sig1 = Alp*sig1
-            sig2 = Alp*sig2
+c            sig2 = Alp*sig2
 
             azmth = 2.0d0*pi
             pf = 0.5d0*rsp
             ps2 = 1.0d0/(4.d0*pi*pi)*(pf/4.d0/rsp)*azmth
 
             wgt1 = sig1/flux_1*ps2*xjac*vwgt
-            wgt2 = sig2/flux_1*ps2*xjac*vwgt
+c            wgt2 = sig2/flux_1*ps2*xjac*vwgt
             PKplus_1 = xnorm*wgt1/vwgt/2d0/eps
-            PKDel = xnorm*wgt2/vwgt/2d0/eps
+c            PKDel = xnorm*wgt2/vwgt/2d0/eps
 
 c        write(*,*)'PKplus =', PKplus_1
          endif
 
 c  102     continue
 
-         PKterms = PKplus_x - PKplus_1 + PKDel
-c         PKterms = PKplus_x - PKplus_1
+c         PKterms = PKplus_x - PKplus_1 + PKDel
+         PKterms = PKplus_x - PKplus_1
 c        PKterms = PKDel
 
 
@@ -157,6 +157,85 @@ c        PKterms = PKDel
 
       return
       end
+c---------------------------------------------------------------------
+c Delta terms
+c---------------------------------------------------------------------
+      function flo2_PKDel(yy,vwgt)
+      implicit double precision (a-h,o-z)
+      dimension yy(10)
+      dimension f1(-6:6),f2(-6:6),xl(15)
+      dimension p1(0:3),p2(0:3),p3(0:3),p4(0:3),q(0:3),xp1(0:3),xp2(0:3)
+     .          ,p(0:3,1:4),Born(1:2)
+      dimension SumP(1:2),SumK(1:2)
+      parameter (pi=3.14159265358979d0)
+      parameter (hbarc2=389.3856741D+6)
+      character*50 name
+      common/energy/s
+      common/pdfname/name
+      common/factscale/xmuf
+      common/leg_choice/leg
+      common/usedalpha/AL,ge
+      common/distribution/xq
+
+      xa     = yy(1)
+      xb     = yy(2)
+
+      sp     = xa*xb*s
+      rsp    = dsqrt(sp) 
+      xjac = 1.0d0
+      xnorm=hbarc2
+
+      eps = 0.5d0
+      xlow = xq - eps
+      xhigh = xq + eps
+
+        call kinvar2(yy,xinvmass,p1,p2,p3,p4)
+
+        call p1d_to_p2d_4(p1,p2,p3,p4,p)
+
+        pin  = 0.5d0*rsp
+        flux_1 = 4.0d0*pin*rsp
+
+        scale2 = 2.0d0*dot(p1,p2)
+        scale = dsqrt(scale2)
+
+      flo2_PKDel  = 0.0d0
+      sig1 = 0d0
+
+
+        if (scale .ge. xlow .and. scale .le. xhigh)  then   
+
+            xmuf = scale
+            xmur = scale
+            AL = alphasPDF(xmur)
+            AS = 1.0d0
+            ALP = AS
+
+            call getPKDel(1.0d0,xmuf,p,p1,p2,SumK)
+                
+            call pdf(xa,xmuf,f1)
+            call pdf(xb,xmuf,f2)
+            call setlum(f1,f2,xl)
+
+            sig1 = xl(1)* SumK(1)  !  [qq lum]
+
+            sig1 = Alp*sig1
+
+            azmth = 2.0d0*pi
+            pf = 0.5d0*rsp
+            ps2 = 1.0d0/(4.d0*pi*pi)*(pf/4.d0/rsp)*azmth
+
+            wgt1 = sig1/flux_1*ps2*xjac*vwgt
+            PKDel = xnorm*wgt1/vwgt/2d0/eps
+
+c        write(*,*)'PKDel =', PKDel
+         endif
+
+         flo2_PKDel = PKDel
+
+      return
+      end
+
 
 c---------------------------------------------------------------------
 c     [u U -> e E]  Born 
